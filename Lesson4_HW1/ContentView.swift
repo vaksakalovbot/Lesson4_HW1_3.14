5531e05bd7a82840cec7ec205a0c2ea4ff92f53d
Vladimir <vaksakalovbot@yandex.ru>
2020-08-02 16:59:05 +0300

    Initial Commit

---
+++ Lesson4_HW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson4_HW1
+//
+//  Created by Vladimir on 02.08.2020.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
